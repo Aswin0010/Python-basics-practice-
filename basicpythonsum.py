7a4abe2578bc6ea34067f74d5a30cd634c8a5994
Aswin0010 <raiaswin650@gmail.com>
2026-04-03 23:13:14 +0530

    Add files via upload

---
+++ basicpythonsum.py
@@ -0,0 +1,5 @@
+#program to ask two numbers from user and add it
+a=int(input("Enter the first number : "))
+b=int(input("Enter the second number : "))
+sum=a+b
+print("The sum of two numbers is :",sum)
