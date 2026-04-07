d2d6e6645b323367e12ecbf2a558a54315b505a9
Aswin0010 <raiaswin650@gmail.com>
2026-04-07 17:56:48 +0530

    Add files via upload

---
+++ string/palindrome.py
@@ -0,0 +1,9 @@
+s=input("Enter the string s : ")
+l=len(s)
+for i in range (0 ,l//2):
+    if(s[i]!=s[l-i-1]):
+        print(s,"is not pallindrom")
+        break
+else:
+    print(s,"is pallindrom")    
+        
