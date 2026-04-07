c3c24b41008be337b5881cf526150a6621af403e
Aswin0010 <raiaswin650@gmail.com>
2026-04-07 17:57:21 +0530

    Update palindrome.py

--- string/palindrome.py
+++ string/palindrome.py
@@ -2,8 +2,8 @@ s=input("Enter the string s : ")
 l=len(s)
 for i in range (0 ,l//2):
     if(s[i]!=s[l-i-1]):
-        print(s,"is not pallindrom")
+        print(s,"is not palindrom")
         break
 else:
-    print(s,"is pallindrom")    
+    print(s,"is palindrom")    
         
