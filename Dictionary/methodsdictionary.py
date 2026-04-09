c41f1bb2d737805fa45cddd42ea75747b6c2f20a
Aswin Rai <raiaswin650@gmail.com>
2026-04-09 20:09:14 +0530

    Add files via upload

---
+++ Dictionary/methodsdictionary.py
@@ -0,0 +1,13 @@
+marks ={
+    "Harry" : 90 ,
+    "Ravi" : 80 ,
+    "Amit" : 85
+    }
+a=marks.get("Harry")
+print(a)
+b=marks.items()
+print(b)
+c=marks.keys()
+print(c)
+d=marks.update({"Harry" : 100})
+print(marks)
